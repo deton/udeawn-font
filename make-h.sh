4c9c53ca82de1ba837b0859e71414e6ab355aaa1
KIHARA Hideto <deton@m1.interq.or.jp>
2023-03-05 12:53:30 +0900

    East Asian Ambiguousなグリフを半分幅に縮めるfontforgeスクリプト分離

--- make-h.sh
+++ make-h.sh
@@ -8,14 +8,13 @@ BUILD_DIR="$BASE_DIR/build"
 VERSION='0.0.6'
 
 FAMILYNAME="UDEAWH"
-DISP_FAMILYNAME="UDEAWH"
 
 if [ ! -d "$BUILD_DIR" ]
 then
   mkdir "$BUILD_DIR"
 fi
 
-"${BASE_DIR}/generator-h.sh" "$VERSION" "$FAMILYNAME" "$DISP_FAMILYNAME"
+"${BASE_DIR}/generator-h.sh" "$VERSION" "$FAMILYNAME"
 "${BASE_DIR}/os2_patch.sh" "$FAMILYNAME" "" 0
 "${BASE_DIR}/copyright-h.sh" "$FAMILYNAME"
 mv "$WORK_DIR/$FAMILYNAME"*.ttf "$BUILD_DIR"
