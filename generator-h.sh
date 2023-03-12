d584da466326d7246707bfa275795764d6aa2664
KIHARA Hideto <deton@m1.interq.or.jp>
2023-03-12 11:39:43 +0900

    試しに、半分幅に縮めた後のttfautohintをコメントアウト

--- generator-h.sh
+++ generator-h.sh
@@ -35,10 +35,10 @@ mv "${FAMILYNAME}-Regular.ttf" "${WORK_DIR}"
 fontforge -script eaa2narrow.pe "$PATH_BIZUD_BOLD" "$FAMILYNAME" Bold "$VERSION"
 mv "${FAMILYNAME}-Bold.ttf" "${WORK_DIR}"
 
-for f in `ls "${WORK_DIR}/${FAMILYNAME}"*.ttf`
-do
-  python3 -m ttfautohint -l 6 -r 45 -a nnn -D latn -f none -S -W -X "13-" -I "$f" "${f}_hinted"
-done
+#for f in `ls "${WORK_DIR}/${FAMILYNAME}"*.ttf`
+#do
+#  python3 -m ttfautohint -l 6 -r 45 -a nnn -D latn -f none -S -W -X "13-" -I "$f" "${f}_hinted"
+#done
 
-mv "${WORK_DIR}/${FAMILYNAME}-Regular.ttf_hinted" "${WORK_DIR}/${MODIFIED_FONT_BIZUD_REGULAR}"
-mv "${WORK_DIR}/${FAMILYNAME}-Bold.ttf_hinted" "${WORK_DIR}/${MODIFIED_FONT_BIZUD_BOLD}"
+#mv "${WORK_DIR}/${FAMILYNAME}-Regular.ttf_hinted" "${WORK_DIR}/${MODIFIED_FONT_BIZUD_REGULAR}"
+#mv "${WORK_DIR}/${FAMILYNAME}-Bold.ttf_hinted" "${WORK_DIR}/${MODIFIED_FONT_BIZUD_BOLD}"
